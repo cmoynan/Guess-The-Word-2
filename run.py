4341f3f7c1a8d563f175f9d5d9af313406cbb604
Chris Moynan <Chrismoynan@outlook.com>
2024-08-19 16:38:16 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+words = [
+    "apple", "banana", "orange", "strawberry", "grape",
+    "pineapple", "watermelon", "mango", "melon", "cranberry"
+]

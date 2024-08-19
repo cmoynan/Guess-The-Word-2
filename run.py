09fb5075dc927c21b5b37d9e450468428cf2dab6
Chris Moynan <Chrismoynan@outlook.com>
2024-08-19 16:47:00 +0000

    added function to randomise words

--- run.py
+++ run.py
@@ -2,5 +2,19 @@ import random
 
 words = [
     "apple", "banana", "orange", "strawberry", "grape",
-    "pineapple", "watermelon", "mango", "melon", "cranberry"
+    "pineapple", "watermelon", "mango", "melon", "cranberry",
+    "blueberry", "kiwi", "pear", "peach", "plum",
+    "apricot", "cherry", "pomegranate", "coconut", "papaya"
 ]
+guessed_words = []
+
+def choose_word(words):
+    """
+    Randomly chooses a word from the array to begin the game.
+    """
+    chosen_word = random.choice(words)
+
+    # Remove the chosen word from the list to prevent repetition
+    words.remove(chosen_word)
+    return chosen_word
+
